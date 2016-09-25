d799d2c302e463aa23bb1d5d1bf522b3fb20bced
Joseph Marchese <joeymarchese@gmail.com>
2016-09-25 14:39:16 -0400

    Add Script File
    Couple Hours of Work. Can't decide how to handle some of the
    cross-capable people in their positions. Will work on tomorrow or
    tonight.

---
+++ masetosscheduler.py
@@ -0,0 +1,131 @@
+import random
+
+class Day():
+    
+    def __init__(self, Name):
+        self.Name = Name
+        self.AMSandwichMaker = ''
+        self.AMGrill = ''
+        self.AMHelper = ''
+        self.AMServers = []
+        self.PMSandwichMaker = ''
+        self.PMGrill = ''
+        self.PMHelper = ''
+        self.PMServers = []
+        self.PMDinners = ''
+        self.trainees = []
+        
+    def GenerateAndSetServers(self, ServerList, Total):
+        servers = random.sample(ServerList, Total)
+        self.AMServers = servers[0:int(Total/2)]
+        self.PMServers = servers[int(Total/2):Total]
+        
+    def GenerateAndSetKitchen(self, SandwichMakerList, GrillerList, HelperList, DinnerList):
+        # Set SandwichMaker
+        self.PMSandwichMaker = random.choice(SandwichMakerList)
+        
+        # Set Griller
+        if self.PMSandwichMaker in GrillerList:
+            GrillerList.remove(self.PMSandwichMaker)            
+        self.PMGrill = random.choice(GrillerList)
+        
+        #Set Helpers
+        for person in [self.PMSandwichMaker, self.PMGrill]:
+            if person in HelperList:
+                HelperList.remove(person)
+        self.AMHelper, self.PMHelper = random.sample(HelperList, 2)
+        
+        #Set Dinners
+        for person in [self.PMSandwichMaker, self.PMGrill, self.AMHelper, self.PMHelper]:
+            if person in DinnerList:
+                DinnerList.remove(person)
+        self.PMDinners = random.choice(DinnerList)
+   
+
+class Person():
+
+    def __init__(self, Name):
+        self.Name = Name
+        self.CanDoSandwiches = None
+        self.CanDoGrill = None
+        self.CanDoHelper = None
+        self.CanServe = None
+        self.CanDoDinners = None
+        
+        
+Monday = Day('Monday')
+Tuesday = Day('Tuesday')
+Wednesday = Day('Wednesday')
+Thursday = Day('Thursday')
+Friday = Day('Friday')
+Saturday = Day('Saturday')
+Week = [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday]
+
+Tammy = Person('Tammy')
+Lisa = Person('Lisa')
+Sherie = Person('Sherie')
+Tim = Person('Tim')
+Peggy = Person('Peggy')
+Katie = Person('Katie')
+Alex = Person('Alex')
+Jamie = Person('Jamie')
+Rhiannon = Person('Rhiannon')
+Kara = Person('Kara')
+Nathan = Person('Nathan')
+Johnny = Person('Johnny')
+Joe = Person('Joe')
+Sara = Person('Sara')
+
+# Leaving only People whose schedules are flexible.
+# Consider dividing into PM and AM Servers / Grillers.
+People = [Tammy, Lisa, Sherie, Tim, Peggy, Katie, Alex,
+          Jamie, Rhiannon, Kara, Nathan, Johnny, Joe, Sara]
+SandwichMakers = [Katie, Rhiannon, Joe]
+Grillers = [Katie, Alex, Rhiannon, Nathan, Johnny, Joe]
+Helpers = [Katie, Alex, Rhiannon, Kara, Nathan, Johnny, Joe]
+Servers = [Tammy, Sherie, Peggy, Jamie, Rhiannon, Kara, Nathan, Sara]
+Dinners = [Alex, Rhiannon, Kara, Nathan, Johnny, Joe]
+
+for sandwichMaker in SandwichMakers:
+    sandwichMaker.CanDoSandwiches = True
+for griller in Grillers:
+    griller.CanDoGrill = True
+for helper in Helpers:
+    helper.CanDoHelper = True
+for server in Servers:
+    server.CanServe = True
+for dinner in Dinners:
+    dinner.CanDoDinner = True
+    
+# Standard Schedule Setup
+for day in Week:
+    day.AMSandwichMaker = Lisa
+    day.AMGrill = Tim
+    day.AMHelper = random.choice(Helpers)
+    day.GenerateAndSetServers(Servers, 2)
+    if day in [Wednesday, Thursday, Friday]:
+        day.GenerateAndSetKitchen(SandwichMakers, Grillers, Helpers, Dinners)
+        day.GenerateAndSetServers(Servers, 4)
+        if day == Friday:
+            day.GenerateAndSetServers(Servers, 6)
+    if day == Saturday:
+        day.AMSandwichMaker, day.PMSandwichMaker = random.sample(SandwichMakers, 2)
+        day.AMGrill, day.PMGrill = random.sample(Grillers, 2) 
+        day.GenerateAndSetServers(Servers, 4)
+
+# Schedule Print Out        
+for day in Week:    
+    print('--', day.Name,' Staff --')
+    print('AMSandwichMaker: ', day.AMSandwichMaker.Name)
+    print('AMGrill: ', day.AMGrill.Name)
+    print('AMHelper: ', day.AMHelper.Name)
+    for server in day.AMServers:
+        print('AMServer: ', server.Name)
+    if day in [Wednesday, Thursday, Friday, Saturday]:
+        print('PMSandwichMaker: ', day.PMSandwichMaker.Name)
+        print('PMGrill: ', day.PMGrill.Name)
+        print('PMHelper: ', day.PMHelper.Name)
+        print('PMDinners: ', day.PMDinners.Name)
+        for server in day.PMServers:
+            print('PMServer: ', server.Name)
+    print('\n')
