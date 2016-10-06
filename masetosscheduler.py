52ccd70b521bd44daee761f9bbdea9ae17efbe1f
Joseph Marchese <joeymarchese@gmail.com>
2016-10-06 00:31:22 -0400

    This is starting to look like other people projects. Should get some help or a code review.

--- masetosscheduler.py
+++ masetosscheduler.py
@@ -1,38 +1,41 @@
 from day import Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
 
 
-def set_schedule(week_list):
-    for day in week_list:
-        day.setup()
-        if day.empty_positions > 0:  # Unsuccessful
-            return False
-    return True  # Successful
+class Schedule:
+    def __init__(self):
+        self.week = self.initialize_week()
 
 
-def print_schedule(week_list):
-    for day in week_list:
-        day.print_day_schedule()
+    def initialize_week(self):
+        week = [Monday('Monday'), Tuesday('Tuesday'), Wednesday('Wednesday'),
+                Thursday('Thursday'), Friday('Friday'), Saturday('Saturday')]
+        return week
 
 
-def initialize_week():
-    week = [Monday('Monday'), Tuesday('Tuesday'), Wednesday('Wednesday'),
-            Thursday('Thursday'), Friday('Friday'), Saturday('Saturday')]
-    return week
+    def set_schedule(self):
+        for day in self.week:
+            day.setup()
+            if day.empty_positions > 0:  # Unsuccessful
+                return False
+        return True  # Successful
 
 
-def run_once_and_print():
-    week = initialize_week()
-    set_schedule(week)
-    print_schedule(week)
+    def generate_schedule(self):
+        self.week = self.initialize_week()
+        return self.set_schedule()
 
 
-def run_until_completed_and_print():
-    week = initialize_week()
-    schedule_completed = set_schedule(week)
-    while not schedule_completed:
-        week = initialize_week()
-        schedule_completed = set_schedule(week)
-    print_schedule(week)
+    def generate_filled_schedule(self):
+        schedule_completed = self.generate_schedule()
+        while not schedule_completed:
+            schedule_completed = self.generate_schedule()
 
 
-run_until_completed_and_print()
+    def print_schedule(self):
+        for day in self.week:
+            day.print_day_schedule()
+
+
+schedule = Schedule()
+schedule.generate_filled_schedule()
+schedule.print_schedule()
